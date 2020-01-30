e2d906679a3eb997fcd7c5de09d8229c9d556d24
urobin84 <urobin@gmail.com>
2020-01-30 17:03:49 +0700

    ahp

--- app/src/main/java/com/mqr/loginform/activity/ReimbursementActivity.kt
+++ app/src/main/java/com/mqr/loginform/activity/ReimbursementActivity.kt
@@ -12,6 +12,7 @@ import com.mqr.loginform.R
 import com.mqr.loginform.ui.ReimbursementAFragment
 import com.mqr.loginform.ui.ReimbursementRFragment
 import kotlinx.android.synthetic.main.activity_reimbursement_menu.*
+import kotlinx.android.synthetic.main.item_reimbursement_tipe.view.*
 
 class ReimbursementActivity : AppCompatActivity()  {
 
@@ -38,7 +39,7 @@ class ReimbursementActivity : AppCompatActivity()  {
         mSectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
 
         // Set up the ViewPager with the sections adapter.
-        container.adapter = mSectionsPagerAdapter
+        VPContainerReimbursement.adapter = mSectionsPagerAdapter
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -58,7 +59,7 @@ class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
     override fun getCount(): Int {
         // Show 5 total pages.(we will use 5 pages so change it to 5)
-        return 5
+        return 3
     }
 }
 
@@ -92,51 +93,30 @@ class PlaceholderFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val rootView = inflater.inflate(R.layout.ite, container, false)
+        val rootView = inflater.inflate(R.layout.item_reimbursement_tipe, container, false)
         /*since our views are in fragment_main.xml which is inflated in rootView
           so we have to write rootView.oursomeview
           otherwise it will try to find the view in activity_main.xml so app will crash*/
         //handle swipe/slide
         if (arguments!!.getInt(ARG_SECTION_NUMBER) == 1){
             //set title to title_tv
-            rootView.title_tv.text = "Title One"
-            //set image to image_iv
-            rootView.image_iv.setImageResource(R.drawable.battery)
+            rootView.reimbursementNominal.text = "Rp 28.066.400"
             //set description to description_tv
-            rootView.description_tv.text = "The long description of the Title One"
+            rootView.reimbursementketerangan.text = "Medical Claime OutPatient"
         }
         if (arguments!!.getInt(ARG_SECTION_NUMBER) == 2){
             //set title to title_tv
-            rootView.title_tv.text = "Title Two"
-            //set image to image_iv
-            rootView.image_iv.setImageResource(R.drawable.cpu)
+            rootView.reimbursementNominal.text = "Rp 28.066.400"
             //set description to description_tv
-            rootView.description_tv.text = "The long description of the Title Two"
+            rootView.reimbursementketerangan.text = "Medical Claime OutPatient"
         }
         if (arguments!!.getInt(ARG_SECTION_NUMBER) == 3){
             //set title to title_tv
-            rootView.title_tv.text = "Title Three"
-            //set image to image_iv
-            rootView.image_iv.setImageResource(R.drawable.devid)
+            rootView.reimbursementNominal.text = "Rp 28.066.400"
             //set description to description_tv
-            rootView.description_tv.text = "The long description of the Title Three"
+            rootView.reimbursementketerangan.text = "Medical Claime OutPatient"
         }
-        if (arguments!!.getInt(ARG_SECTION_NUMBER) == 4){
-            //set title to title_tv
-            rootView.title_tv.text = "Title Four"
-            //set image to image_iv
-            rootView.image_iv.setImageResource(R.drawable.display)
-            //set description to description_tv
-            rootView.description_tv.text = "The long description of the Title Four"
-        }
-        if (arguments!!.getInt(ARG_SECTION_NUMBER) == 5){
-            //set title to title_tv
-            rootView.title_tv.text = "Title Five"
-            //set image to image_iv
-            rootView.image_iv.setImageResource(R.drawable.memory)
-            //set description to description_tv
-            rootView.description_tv.text = "The long description of the Title Five"
-        }
+
         return rootView
     }
 
